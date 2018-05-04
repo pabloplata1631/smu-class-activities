f8dd28e208f2314d12b9cf806260537dd8f56faa
Pablo Plata <pplata1631@gmail.com>
2018-05-03 20:15:33 -0500

    testing

--- Untitled.sql
+++
@@ -1,2 +0,0 @@
-use dallas;
-select * from colleges;
--- schema.sql
+++
@@ -1,9 +0,0 @@
-drop database if exists dallas; 
-create database  dallas;
-use dallas;
-
-create table colleges (
-id int not null auto_increment,
-school_name varchar(100) not null,
-primary key (id)
-);
